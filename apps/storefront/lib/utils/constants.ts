77b88e1e299880b651b02d4650ed21f4af9f7dd1
Harshit Jaiswar <artharshitj@gmail.com>
2026-06-14 16:33:42 +0530

    Setup storefront foundation

---
+++ apps/storefront/lib/utils/constants.ts
@@ -0,0 +1,10 @@
+export const BRAND = {
+  name: "KAPI PEN",
+  tagline: "Pens, Stationery & Art Supplies",
+
+  colors: {
+    primary: "#E53C3C",
+    secondary: "#FFB800",
+    accent: "#4AC5D1",
+  },
+};
